db2af9c7c2506f5a408dabeaf02ddc955b443719
Alfred Gaillard <alfred.gaillard@etudiant.univ-lr.fr>
2021-07-11 19:48:12 +0200

    Add a text on the ui wich displays in real time the input value and automation of the text recovery

--- react-blocknote/src/App.js
+++ react-blocknote/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers'
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import Header from './Header';
+
+const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
 
 function App() {
+  // store greetingInputValue in local state
+  const [greetingInputValue, setGreetingInputValue] = useState()
+  const [greeting, setGreetingValue] = useState();
+
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greetingInputValue value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        /*await provider._getAddress().finally(onfinally?: () => {
+          const providerAdress
+        })*/
+        console.log('data: ', data)
+        //console.log('providerAdress : ', providerAdress)
+        setGreetingValue(data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  // call the smart contract, send an update
+  async function setGreeting() {
+    if (!greetingInputValue) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greetingInputValue)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
+  useEffect(()=>{
+    fetchGreeting()
+  }, [greeting])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header setGreeting={setGreeting} setGreetingInputValue={setGreetingInputValue} fetchGreeting={fetchGreeting}/>
+      <p>{greeting}</p>
+      <p>{greetingInputValue}</p>
     </div>
   );
 }
